d09f05772c06607e25638b6934fae3a9ce7070c2
Faisal Bhat <plesim18@gmail.com>
2023-12-25 05:59:53 +0000

    Used Cors

--- smartBrain-api/server.js
+++ smartBrain-api/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
 const app = express();
 
 app.use(bodyParser.json())
+app.use(cors())
 const database = {
     users:[
         {
@@ -40,13 +42,13 @@ app.get('/',(req,res)=>{
 app.post('/signin',(req,res)=>{
 
     // load hash from your password db
-    bcrypt.compare("apples",hash,function(err,hash){
-        // res = true
-    })
+    // bcrypt.compare("apples",hash,function(err,hash){
+    //     // res = true
+    // })
 
-    bcrypt.compare("veggies",hash,function(err,res){
-        // res = false
-    })
+    // bcrypt.compare("veggies",hash,function(err,res){
+    //     // res = false
+    // })
     // first parse the data
     if (req.body.email === database.users[0].email && req.body.password === database.users[0].password){
         res.json('success')
@@ -58,9 +60,9 @@ app.post('/signin',(req,res)=>{
 
 app.post('/register',(req,res)=>{
     const {email,name,password} = req.body
-    bcrypt.hash(password,null,null,function(err,hash){
-        console.log(hash)
-    })
+    // bcrypt.hash(password,null,null,function(err,hash){
+    //     console.log(hash)
+    // })
     database.users.push({
         id:'125',
         name:name,
--- smartBrain/src/App.js
+++ smartBrain/src/App.js
@@ -27,6 +27,12 @@ class App extends Component {
     };
   }
 
+  componentDidMount(){
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
